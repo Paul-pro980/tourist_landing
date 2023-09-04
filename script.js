1c46732149c51bb9d947620279609ca2d3387fd1
P.V.Musalyamov <83179890+Paul-pro980@users.noreply.github.com>
2023-09-04 19:38:09 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', scrollToSection);
+});
+
+function scrollToSection(event) {
+  event.preventDefault();
+
+  const targetId = event.target.getAttribute('href').substring(1);
+
+  const targetSection = document.getElementById(targetId);
+
+  if (targetSection) {
+    targetSection.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+}
+
+const form = document.getElementById('feedbackForm');
+const testimonialsSection = document.querySelector('.testimonials');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const nameInput = form.querySelector('input[type="text"]').value;
+  const messageInput = form.querySelector('textarea').value;
+
+  const testimonial = document.createElement('div');
+  testimonial.classList.add('testimonial');
+  testimonial.innerHTML = `
+    <p class="content">${messageInput}</p>
+    <p class="author">${nameInput}</p>
+  `;
+  testimonialsSection.appendChild(testimonial);
+  form.reset();
+});
+
+
+{/* <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  $(document).ready(function() {
+    $('.package').hover(
+      function() {
+        $(this).find('.description, .price').slideDown();
+      },
+      function() {
+        $(this).find('.description, .price').slideUp();
+      }
+    );
+  });
+ */}
+  
